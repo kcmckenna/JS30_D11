f445aaf480b57306ce6fd14722697e2c760350b0
Kevin C McKenna <kevmckenna@gmail.com>
2017-12-02 15:54:43 -0800

    building playback function

---
+++ scripts.js
@@ -0,0 +1,38 @@
+/* Get Elements */
+
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+
+/* Build Functions */
+
+function togglePlay() {
+    // const method = video.paused ? 'play' : 'pause';
+    // video[method]();
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+
+}
+
+function updateButton() {
+    const icon = this.pause ? '►' : '❚ ❚';
+    toggle.textContent = icon;
+    console.log('Update button')
+}
+
+/* Sync Event Listeners */
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+toggle.addEventListener('click', togglePlay);
