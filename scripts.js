bee5f450c3dfe5245054dc9b14658e7f1f41c7aa
Kevin C McKenna <kevmckenna@gmail.com>
2017-12-02 16:13:55 -0800

    buidling progress bar functionality

--- scripts.js
+++ scripts.js
@@ -24,9 +24,26 @@ function togglePlay() {
 }
 
 function updateButton() {
-    const icon = this.pause ? '►' : '❚ ❚';
+    const icon = this.paused ? '►' : '❚ ❚';
     toggle.textContent = icon;
     console.log('Update button')
+}
+
+function skip() {
+    console.log('skipping!');
+    console.log(this.dataset.skip);
+    video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+    console.log(this.name);
+    console.log(this.value);
+}
+
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
 }
 
 /* Sync Event Listeners */
@@ -36,3 +53,6 @@ video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 
 toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
